7eb9b76d2e9bc00be14844c9a2d15c87651d9065
James Lee <uptownhr@gmail.com>
2016-10-04 23:24:22 -0700

    added scrolled animation

--- webpack/src/index.js
+++ webpack/src/index.js
@@ -293,4 +293,23 @@ Vue.component('user-list', {
   }
 })
 
+$(window).on('scroll', function(){
+  var top = $(window).scrollTop()
+
+  if (top > 25 ) {
+    $('.is-brand').addClass('has-scrolled')
+  }else{
+    $('.is-brand').removeClass('has-scrolled')
+  }
+
+  var prices_top = $('.fold .check-prices').offset().top
+  console.log(top, prices_top)
+
+  if(top > prices_top){
+    $('.nav .check-prices').addClass('scrolled')
+  }else{
+    $('.nav .check-prices').removeClass('scrolled')
+  }
+})
+
 window.Vue = Vue;
